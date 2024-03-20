fbf0807fb15923ae86fe4cb4c77b47859a0550e0
Eric Bernard <eric99b@gmail.com>
2024-03-19 20:51:59 -0400

    Undo formatting

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,33 +1,33 @@
-import { Moment } from "moment-timezone"
-import { FC } from "react"
+import { Moment } from 'moment-timezone';
+import { FC } from 'react';
 
 export interface CalendarProps {
-	apiKey: string
-	calendars: {
-		calendarId: string
-		color?: string
-	}[]
-	language?: string
-	styles?: object
-	showArrow?: boolean
-	showFooter?: boolean
+  apiKey: string;
+  calendars: {
+    calendarId: string,
+    color?: string,
+  }[];
+  language?: string;
+  styles?: object;
+  showArrow?: boolean;
+  showFooter?: boolean;
 }
 
 export interface CalendarState {
-	monthNames: string[]
-	days: string[]
-	today: Moment
-	current: Moment
-	calendars: {
-		calendarId: string
-		color?: string
-	}[]
-	events: any[]
-	singleEvents: any[]
-	userTimezone: string
-	showArrow: boolean
-	showFooter: boolean
-	processedCalendars: string[]
+  monthNames: string[];
+  days: string[];
+  today: Moment;
+  current: Moment;
+  calendars: {
+    calendarId: string,
+    color?: string,
+  }[];
+  events: any[];
+  singleEvents: any[];
+  userTimezone: string;
+  showArrow: boolean;
+  showFooter: boolean;
+  processedCalendars: string[];
 }
 
 declare const CalendarComponent: FC<CalendarProps>
