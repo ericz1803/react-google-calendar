3c7af412983eeb4a4ed919280ec121532535de68
Eric Bernard <eric99b@gmail.com>
2024-03-19 20:45:09 -0400

    Add default export to `index.d.ts` to match library

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,30 +1,34 @@
-import { Moment } from 'moment-timezone';
+import { Moment } from "moment-timezone"
+import { FC } from "react"
 
 export interface CalendarProps {
-  apiKey: string;
-  calendars: {
-    calendarId: string,
-    color?: string,
-  }[];
-  language?: string;
-  styles?: object;
-  showArrow?: boolean;
-  showFooter?: boolean;
+	apiKey: string
+	calendars: {
+		calendarId: string
+		color?: string
+	}[]
+	language?: string
+	styles?: object
+	showArrow?: boolean
+	showFooter?: boolean
 }
 
 export interface CalendarState {
-  monthNames: string[];
-  days: string[];
-  today: Moment;
-  current: Moment;
-  calendars: {
-    calendarId: string,
-    color?: string,
-  }[];
-  events: any[];
-  singleEvents: any[];
-  userTimezone: string;
-  showArrow: boolean;
-  showFooter: boolean;
-  processedCalendars: string[];
+	monthNames: string[]
+	days: string[]
+	today: Moment
+	current: Moment
+	calendars: {
+		calendarId: string
+		color?: string
+	}[]
+	events: any[]
+	singleEvents: any[]
+	userTimezone: string
+	showArrow: boolean
+	showFooter: boolean
+	processedCalendars: string[]
 }
+
+declare const CalendarComponent: FC<CalendarProps>
+export default CalendarComponent
